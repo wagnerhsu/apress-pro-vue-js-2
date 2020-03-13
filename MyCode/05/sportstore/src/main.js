a7eac543f79deba38f52f34e54d4a6212cae3434
Wagner Xu <wagnerhsu@hotmail.com>
2020-03-13 10:52:48 +0800

    Add files

--- MyCode/05/sportstore/src/main.js
+++ MyCode/05/sportstore/src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import "bootstrap/dist/css/bootstrap.css"
+import "font-awesome/css/font-awesome.css"
+
+import store from "./store";
+
 new Vue({
   render: h => h(App),
+  store
 }).$mount('#app')
---
+++ MyCode/05/sportstore/src/store/index.js
@@ -0,0 +1,42 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+const testData = [];
+
+for (let i = 1; i <= 10; i++) {
+    testData.push({
+        id: i,
+        name: `Product #${i}`,
+        category: `Category ${i % i}`,
+        description: `This is Product #${i}`,
+        price: i * 50
+    })
+}
+
+export default new Vuex.Store({
+    strict: true,
+    state: {
+        products: testData,
+        productsTotal: testData.length,
+        currentPage: 1,
+        pageSize: 4
+    },
+    getters: {
+        processedProducts: state => {
+            let index = (state.currentPage - 1) * state.pageSize;
+            return state.products.slice(index, index + state.pageSize)
+        },
+        pageCount: state => Math.ceil(state.productsTotal / state.pageSize)
+    },
+    mutations: {
+        setCurrentPage(state, page) {
+            state.currentPage = page;
+        },
+        setPageSize(state, size) {
+            state.pageSize = size;
+            state.currentPage = 1;
+        }
+    }
+})
