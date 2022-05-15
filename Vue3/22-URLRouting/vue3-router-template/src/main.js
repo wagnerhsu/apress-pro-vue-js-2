4beb6b3d7190a98f6f4b1755972a5f11b803434f
Wagner Xu <wagnerhsu@hotmail.com>
2022-05-15 23:02:26 +0800

    update files

--- Vue3/22-URLRouting/vue3-router-template/src/main.js
+++ Vue3/22-URLRouting/vue3-router-template/src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).mount('#app');
+import 'bootstrap/dist/js/bootstrap.js';
