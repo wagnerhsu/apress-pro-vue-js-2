f66774ebbbb3224bf1d91395727a218d52761acf
Wagner Xu <wagnerhsu@hotmail.com>
2022-05-21 23:16:44 +0800

    update files

---
+++ Vue3/20-DataStore/productapp/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col"><error-display /></div>
+    </div>
+    <div class="row">
+      <div class="col-8 m-3"><product-display /></div>
+      <div class="col m-3"><product-editor /></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ProductDisplay from './components/ProductDisplay';
+import ProductEditor from './components/ProductEditor';
+import ErrorDisplay from './components/ErrorDisplay';
+</script>
