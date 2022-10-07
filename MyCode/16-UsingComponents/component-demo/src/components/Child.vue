c10ae0dc09da74e9eb672d1f6591cfa7fac236cf
Wagner Xu <wagnerhsu@hotmail.com>
2022-10-07 22:10:47 +0800

    update files

---
+++ MyCode/16-UsingComponents/component-demo/src/components/Child.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="bg-primary text-white text-center m-2 p-2 h6">
+        {{message}}
+        <div class="form-group m-1 text-start">
+        <label>{{labelText}}</label>
+        <input v-model="message" class="form-control" />
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+const props = defineProps({
+    labelText: String,
+    initialValue: String
+})
+const message = ref(props.initialValue)
+</script>
