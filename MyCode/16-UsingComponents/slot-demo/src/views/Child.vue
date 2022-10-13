6fe5bca660fb10705955a2a79e4366146fb3716e
Wagner Xu <wagnerhsu@hotmail.com>
2022-10-13 22:44:30 +0800

    update files

---
+++ MyCode/16-UsingComponents/slot-demo/src/views/Child.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-primary text-white text-center m-2 p-3 h6">
+    <slot name="header">
+      <h4>Use the form fields to edit the data</h4>
+    </slot>
+    <slot>
+      <h4>Use the form fields to edit the data</h4>
+    </slot>
+    <slot name="footer">footer</slot>
+  </div>
+</template>
