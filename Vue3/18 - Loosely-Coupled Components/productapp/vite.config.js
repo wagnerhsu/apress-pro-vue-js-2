f5251030563105f41ff2da5686f7d97b9d91777d
Wagner Xu <wagnerhsu@hotmail.com>
2026-04-30 16:46:56 +0800

    refactor: Refactor project structure to use Vite for build and development
    - Updated package.json files across multiple projects to replace Vue CLI with Vite for serving and building applications.
    - Added necessary dependencies for Vite, including @vitejs/plugin-vue and @vue/compiler-sfc.
    - Configured Vite with appropriate plugins and resolve options to support Vue 3 compatibility.
    - Created index.html files for various projects to serve as entry points for the applications.
    - Ensured compatibility with Vue 3 by including @vue/compat in dependencies where necessary.

--- Vue3/18 - Loosely-Coupled Components/productapp/vite.config.js
+++ Vue3/18 - Loosely-Coupled Components/productapp/vite.config.js
@@ -2,7 +2,23 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          compatConfig: {
+            MODE: 2
+          }
+        }
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      vue: '@vue/compat'
+    },
+    extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json', '.vue']
+  },
   server: {
     port: 8080
   }
